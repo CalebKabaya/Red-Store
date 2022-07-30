2c10214645b6d2118a92847bdd20a7381d83dbe0
caleb <calebmutuku4@gmail.com>
2022-07-30 10:53:19 +0300

    reqistered the models in the admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User,Customer,Client
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Client)
+admin.site.register(Customer)
