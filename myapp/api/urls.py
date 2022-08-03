29641caddb10205a6db5bbd9e05cfe9ceeb489e3
caleb <calebmutuku4@gmail.com>
2022-08-03 18:52:35 +0300

    bug fix on my app urls

--- myapp/api/urls.py
+++ myapp/api/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 
 urlpatterns = [
-    # path('signup/client/',ClientView.as_view()),
-    # path('signup/customer/',CustomerView.as_view()),
-    # path('login/',CustomAuthToken.as_view(),name='auth-token'),
-    # path('customer/dashboard/',CustomerOnlyView.as_view(),name='customer-dashboard'),
-    # path('client/dashboard/',ClientOnlyView.as_view,name='client-dashboard')
+    path('signup/client/',ClientView.as_view()),
+    path('signup/customer/',CustomerView.as_view()),
+    path('login/',CustomAuthToken.as_view(),name='auth-token'),
+    path('customer/dashboard/',CustomerOnlyView.as_view(),name='customer-dashboard'),
+    path('client/dashboard/',ClientOnlyView.as_view,name='client-dashboard')
 ]
